5a827a34f8294ef0ba44e1abffd870f55c060dd4
Martynas Kanapinskas <martis347@gmail.com>
2017-01-29 21:40:40 +0200

    Added translations support

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import Navigation from '../containers/NavigationContainer';
+import {TranslatorProvider} from "react-translate"
 
 class App extends React.Component {
+
+  translations = {
+    locale: "en",
+    NavigationContainer: {
+      HOME: "Namai",
+      CLOCK: "Laikrodis",
+      CONTROLS: "Valdikliai",
+      ABOUT: "Kontaktai"
+    },
+  };
+
   render() {
     return (
-      <div>
-        <Navigation />
-        {this.props.children}
-      </div>
+      <TranslatorProvider translations={this.translations}>
+        <div>
+          <Navigation />
+          {this.props.children}
+        </div>
+      </TranslatorProvider>
     );
   }
 }
--- src/containers/NavigationContainer.js
+++ src/containers/NavigationContainer.js
@@ -4,8 +4,10 @@ import NavigationComponent from '../components/navigation/NavigationComponent';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as navigationActions from '../actions/navigationActions';
-import { Swipeable  } from 'react-touch';
+import {Swipeable} from 'react-touch';
+import {translate} from "react-translate";
 
+@translate("NavigationContainer")
 class NavigationContainer extends React.Component {
   constructor(props) {
     super(props);
@@ -13,33 +15,36 @@ class NavigationContainer extends React.Component {
     this.toggleNavigation = this.toggleNavigation.bind(this);
   }
 
-  toggleNavigation(close){
+  toggleNavigation(close) {
     this.props.actions.toggle(close);
   }
 
   render() {
+    const {t} = this.props;
+
     return (
-    <Swipeable onSwipeRight={() => this.toggleNavigation(false)}>
-      <div
-        onMouseEnter={() => this.toggleNavigation(false)}
-        onMouseLeave={() => this.toggleNavigation(true)}
-        onClick={() => this.toggleNavigation(true)}
-      >
-        <NavigationComponent expand={this.props.expand} >
-          <NavigationItem link="" text="Home" expand={this.props.expand} glyphicon="glyphicon-home"/>
-          <NavigationItem link="clock" text="Clock" expand={this.props.expand} glyphicon="glyphicon-time" />
-          <NavigationItem link="link2" text="Controls" expand={this.props.expand} glyphicon="glyphicon-flash" />
-          <NavigationItem link="link3" text="About" expand={this.props.expand} glyphicon="glyphicon-user" />
-        </NavigationComponent>
-      </div>
-    </Swipeable>
+      <Swipeable onSwipeRight={() => this.toggleNavigation(false)}>
+        <div
+          onMouseEnter={() => this.toggleNavigation(false)}
+          onMouseLeave={() => this.toggleNavigation(true)}
+          onClick={() => this.toggleNavigation(true)}
+        >
+          <NavigationComponent expand={this.props.expand}>
+            <NavigationItem link="" text={t("HOME")} expand={this.props.expand} glyphicon="glyphicon-home"/>
+            <NavigationItem link="clock" text={t("CLOCK")} expand={this.props.expand} glyphicon="glyphicon-time"/>
+            <NavigationItem link="link2" text={t("CONTROLS")} expand={this.props.expand} glyphicon="glyphicon-flash"/>
+            <NavigationItem link="link3" text={t("ABOUT")} expand={this.props.expand} glyphicon="glyphicon-user"/>
+          </NavigationComponent>
+        </div>
+      </Swipeable>
     );
   }
 }
 
 NavigationContainer.propTypes = {
   actions: PropTypes.object.isRequired,
-  expand: PropTypes.bool.isRequired
+  expand: PropTypes.bool.isRequired,
+  t: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state) {
